c7137a9adaa0b81fb9050d9d748b98797882155b
Dev-scott <sadoscott25@gmail.com>
2024-03-19 21:43:40 +0100

    guards

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -5,18 +5,22 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/CreateUser.dto';
+import { AuthGuard } from 'src/users/guards/auth/auth.guard';
 import { ValidateCreateUserPipe } from 'src/users/pipes/validate-create-user/validate-create-user.pipe';
 import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
+
 export class UsersController {
   constructor(private userServicve: UsersService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   getUsers() {
     return this.userServicve.fetchUsers();
   }
