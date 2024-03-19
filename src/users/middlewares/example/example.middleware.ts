c1a2d2128dd94623b6dcf3868ab4a76f22f03952
Dev-scott <sadoscott25@gmail.com>
2024-03-19 07:03:03 +0100

    Work with middleware

--- src/users/middlewares/example/example.middleware.ts
+++ src/users/middlewares/example/example.middleware.ts
@@ -1,9 +1,27 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class ExampleMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: () => void) {
     console.log('Exemple middleware');
-    next();
+
+    console.log(req.headers.authorization);
+    const { authorization } = req.headers;
+
+    if (!authorization)
+      throw new HttpException('No Authorization token', HttpStatus.FORBIDDEN);
+
+    if (authorization) next();
+    else
+      throw new HttpException(
+        'Invalid Authorization token',
+        HttpStatus.FORBIDDEN,
+      );
   }
 }
